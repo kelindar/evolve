7ad4818dccc6acd8473db854e768541edc7d4f21
Roman Atachiants <roman.atachiants@gmail.com>
2023-01-05 10:56:18 +0400

    cleanup generate

--- ffnet/math/generate.sh
+++ ffnet/math/generate.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
+# relevant clang flags:
+# -mavx2: enables AVX2 instructions for the compiled code
+# -masm=intel: specifies to use the Intel syntax for assembly language
+# -mllvm -inline-threshold=1000: tells the compiler to use the LLVM inline threshold of 1000 for optimization
+# -mfma: enables FMA instructions for the compiled code
+# -mstackrealign: tells the compiler to realign the stack before function calls
+# -mno-red-zone: disables the use of the red zone in the compiled code
+# -fno-asynchronous-unwind-tables: tells the compiler not to generate asynchronous unwind tables
+# -fno-exceptions: disables the use of exceptions in the compiled code
+# -fno-rtti: disables the use of RTTI (run-time type information) in the compiled code
+# -ffast-math: enables "unsafe" floating point optimizations
+# -O3: enables level 3 optimization for the compiled code
 PATH="$PATH:./bin"
-LLVM_FLAGS="-mllvm -inline-threshold=1000"
-#CLANG_OPTS="-mno-red-zone -no-integrated-as -mstackrealign -fno-asynchronous-unwind-tables -fno-exceptions -fno-rtti -ffast-math -O3"
-#CLANG_OPTS="-mfma -no-builtin -ffreestanding -mno-red-zone -no-integrated-as -mstackrealign -fno-asynchronous-unwind-tables -fno-exceptions -fno-rtti -ffast-math -O3"
-CLANG_OPTS="-mfma -mstackrealign -fno-asynchronous-unwind-tables -fno-exceptions -fno-rtti -ffast-math -O3"
-
 
 function build_avx2_amd64 {
     SRC="avx2_amd64.cpp"
     ASM="avx2_amd64.s"
-    clang-15 -S -mavx2 -masm=intel $CLANG_OPTS $LLVM_FLAGS -o $ASM $SRC
-    c2goasm -a -f $ASM ../$ASM
-   # rm $ASM
-}
-
-function build_neon_arm64 {
-    SRC="neon_arm64.cpp"
-    ASM="neon_arm64.s"
-    clang-14 -S -arch arm64 $CLANG_OPTS $LLVM_FLAGS -o $ASM $SRC
+    clang-15 -S -o $ASM $SRC \
+        -mavx2 \ 
+        -masm=intel \
+        -mllvm -inline-threshold=1000
+        -mfma \
+        -mstackrealign \
+        -mno-red-zone \
+        -fno-asynchronous-unwind-tables \
+        -fno-exceptions \
+        -fno-rtti \
+        -ffast-math \
+        -O3
     c2goasm -a -f $ASM ../$ASM
     rm $ASM
 }
 
 build_avx2_amd64
-#build_avx512_amd64
-#build_neon_arm64

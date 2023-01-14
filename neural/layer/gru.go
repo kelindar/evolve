d990d9df25320461c184c1f7824e3789e2cb6c5d
Roman Atachiants <roman.atachiants@gmail.com>
2023-01-14 17:14:51 +0400

    rename to mgu

--- neural/layer/gru.go
+++ neural/layer/gru.go
@@ -8,36 +8,37 @@ import (
 	"github.com/kelindar/evolve/neural/math32"
 )
 
-type GRU struct {
-	Wz, Uz, Bz math32.Matrix // Gating unit
-	Wh, Uh, Bh math32.Matrix // Hidden unit
+// MGU is a minimal gated recurrent unit layer
+type MGU struct {
+	Wf, Uf, Bf math32.Matrix // Forget gate
+	Wh, Uh, Bh math32.Matrix // Hidden gate
 	h          math32.Matrix // hidden state
 	hc         math32.Matrix // hidden state candidate
 }
 
-// NewGRU creates a new GRU layer, based on https://arxiv.org/pdf/1803.04831.pdf
-func NewGRU(inputSize, hiddenSize int) *GRU {
-	return &GRU{
-		Wz: math32.NewDenseRandom(inputSize, hiddenSize),
+// NewMGU creates a new MGU layer, based on https://arxiv.org/abs/1603.09420 and https://arxiv.org/abs/1701.03452
+func NewMGU(inputSize, hiddenSize int) *MGU {
+	return &MGU{
+		Wf: math32.NewDenseRandom(inputSize, hiddenSize),
 		Wh: math32.NewDenseRandom(inputSize, hiddenSize),
-		Uz: math32.NewDenseRandom(1, hiddenSize),
+		Uf: math32.NewDenseRandom(1, hiddenSize),
 		Uh: math32.NewDenseRandom(1, hiddenSize),
-		Bz: math32.NewDenseRandom(1, hiddenSize),
+		Bf: math32.NewDenseRandom(1, hiddenSize),
 		Bh: math32.NewDenseRandom(1, hiddenSize),
 		h:  math32.NewDense(1, hiddenSize, nil),
 		hc: math32.NewDense(1, hiddenSize, nil),
 	}
 }
 
-func (l *GRU) Update(z, x *math32.Matrix) *math32.Matrix {
-	z.Reset(x.Rows, l.Wz.Cols)
+func (l *MGU) Update(z, x *math32.Matrix) *math32.Matrix {
+	z.Reset(x.Rows, l.Wf.Cols)
 	l.hc.Zero()
 
-	// Compute the gating unit
-	math32.Matmul(z, x, &l.Wz)
-	math32.Mul(l.h.Data, l.Uz.Data)
+	// Compute the forget gate
+	math32.Matmul(z, x, &l.Wf)
+	math32.Mul(l.h.Data, l.Uf.Data)
 	math32.Add(z.Data, l.h.Data)
-	math32.Add(z.Data, l.Bz.Data)
+	math32.Add(z.Data, l.Bf.Data)
 	math32.Lrelu(z.Data)
 
 	// Compute the hidden unit
@@ -49,14 +50,17 @@ func (l *GRU) Update(z, x *math32.Matrix) *math32.Matrix {
 	math32.Add(hc.Data, l.Bh.Data)
 	math32.Tanh(hc.Data)
 
-	// Multiply the candidate hidden state by the gating unit
+	// Multiply the candidate hidden state by the forget gate
 	math32.Mul(hc.Data, z.Data)
 
+	// Invert the forget gate and multiply it by the previous hidden state
 	for i := range z.Data {
 		z.Data[i] = 1 - z.Data[i]
 	}
 
 	math32.Mul(z.Data, l.h.Data)
+
+	// Add the two together to get the new hidden state
 	math32.Add(z.Data, hc.Data)
 
 	// Remember the hidden state for the next time step
@@ -65,30 +69,30 @@ func (l *GRU) Update(z, x *math32.Matrix) *math32.Matrix {
 }
 
 // Crossover performs crossover between two genomes
-func (l *GRU) Crossover(g1, g2 evolve.Genome) {
-	l1 := g1.(*GRU)
-	l2 := g2.(*GRU)
+func (l *MGU) Crossover(g1, g2 evolve.Genome) {
+	l1 := g1.(*MGU)
+	l2 := g2.(*MGU)
 
 	crossoverMatrix(&l.Wh, &l1.Wh, &l2.Wh)
 	crossoverMatrix(&l.Uh, &l1.Uh, &l2.Uh)
 	crossoverMatrix(&l.Bh, &l1.Bh, &l2.Bh)
-	crossoverMatrix(&l.Wz, &l1.Wz, &l2.Wz)
-	crossoverMatrix(&l.Uz, &l1.Uz, &l2.Uz)
-	crossoverMatrix(&l.Bz, &l1.Bz, &l2.Bz)
+	crossoverMatrix(&l.Wf, &l1.Wf, &l2.Wf)
+	crossoverMatrix(&l.Uf, &l1.Uf, &l2.Uf)
+	crossoverMatrix(&l.Bf, &l1.Bf, &l2.Bf)
 }
 
 // Mutate mutates the genome
-func (l *GRU) Mutate() {
+func (l *MGU) Mutate() {
 	const rate = 0.05
 
 	mutateWeights(l.Wh.Data, rate)
 	mutateWeights(l.Uh.Data, rate)
 	mutateVector(l.Bh.Data, rate)
-	mutateWeights(l.Wz.Data, rate)
-	mutateWeights(l.Uz.Data, rate)
-	mutateVector(l.Bz.Data, rate)
+	mutateWeights(l.Wf.Data, rate)
+	mutateWeights(l.Uf.Data, rate)
+	mutateVector(l.Bf.Data, rate)
 }
 
-func (l *GRU) Reset() {
+func (l *MGU) Reset() {
 	l.h.Zero()
 }

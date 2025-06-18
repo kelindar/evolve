0d573daebd77fa033813acc1f93dd7958d7c5755
Roman Atachiants <roman.atachiants@gmail.com>
2025-06-19 00:05:54 +0400

    Implement proper MGU and update tests

--- neural/layer/mgu.go
+++ neural/layer/mgu.go
@@ -10,16 +10,24 @@ import (
 
 // MGU is a minimal gated recurrent unit layer
 type MGU struct {
-	Uf, Bf math32.Matrix // Forget gate
-	Wh, Bh math32.Matrix // Hidden gate
-	h, hc  math32.Matrix // hidden state and a candidate
+	// Forget gate parameters
+	Wf, Uf, Bf math32.Matrix
+
+	// Candidate state parameters
+	Wh, Uh, Bh math32.Matrix
+
+	// Internal state and scratch space
+	h  math32.Matrix
+	hc math32.Matrix
 }
 
 // NewMGU creates a new MGU layer, based on https://arxiv.org/abs/1603.09420 and https://arxiv.org/abs/1701.03452
 func NewMGU(inputSize, hiddenSize int) *MGU {
 	return &MGU{
+		Wf: math32.NewMatrixRandom(inputSize, hiddenSize),
 		Wh: math32.NewMatrixRandom(inputSize, hiddenSize),
 		Uf: math32.NewMatrixRandom(1, hiddenSize),
+		Uh: math32.NewMatrixRandom(1, hiddenSize),
 		Bf: math32.NewMatrixBias(1, hiddenSize),
 		Bh: math32.NewMatrixBias(1, hiddenSize),
 		h:  math32.NewMatrix(1, hiddenSize, nil),
@@ -27,29 +35,43 @@ func NewMGU(inputSize, hiddenSize int) *MGU {
 	}
 }
 
-func (l *MGU) Update(f, x *math32.Matrix) *math32.Matrix {
-	f.Reset(x.Rows, l.h.Cols)
+func (l *MGU) Update(dst, x *math32.Matrix) *math32.Matrix {
+	dst.Reset(x.Rows, l.h.Cols)
 
-	// f = sigmoid(h_prev ⊙ Uf + Bf)
-	copy(f.Data, l.h.Data)
-	math32.Mul(f.Data, l.Uf.Data)
-	math32.Add(f.Data, l.Bf.Data)
-	math32.Sigmoid(f.Data)
+	// ------------------------------------------------------------------
+	// Forget gate: f_t = σ(W_f·x_t + U_f⊙h_{t-1} + b_f)
+	// ------------------------------------------------------------------
+	math32.Matmul(dst, x, &l.Wf)
+	tmp := append([]float32(nil), l.h.Data...)
+	math32.Mul(tmp, l.Uf.Data)
+	math32.Add(dst.Data, tmp)
+	math32.Add(dst.Data, l.Bf.Data)
+	math32.Sigmoid(dst.Data)
+	f := dst.Data
 
-	// hc = tanh(x·Wh + Bh)
+	// ------------------------------------------------------------------
+	// Candidate state: \tilde{h}_t = tanh(W_h·x_t + U_h⊙(f_t⊙h_{t-1}) + b_h)
+	// ------------------------------------------------------------------
 	hc := &l.hc
 	hc.Reset(x.Rows, l.h.Cols)
 	math32.Matmul(hc, x, &l.Wh)
+
+	tmp = append(tmp[:0], l.h.Data...)
+	math32.Mul(tmp, f)
+	math32.Mul(tmp, l.Uh.Data)
+	math32.Add(hc.Data, tmp)
 	math32.Add(hc.Data, l.Bh.Data)
 	math32.Tanh(hc.Data)
 
-	// h = f ⊙ h_prev + (1 - f) ⊙ hc
-	for i, ft := range f.Data {
-		f.Data[i] = ft*l.h.Data[i] + (1-ft)*hc.Data[i]
+	// ------------------------------------------------------------------
+	// Final state: h_t = (1-f_t)⊙h_{t-1} + f_t⊙\tilde{h}_t
+	// ------------------------------------------------------------------
+	for i, ft := range f {
+		dst.Data[i] = (1-ft)*l.h.Data[i] + ft*hc.Data[i]
 	}
 
-	copy(l.h.Data, f.Data)
-	return f
+	copy(l.h.Data, dst.Data)
+	return dst
 }
 
 // Crossover performs crossover between two genomes
@@ -57,21 +79,26 @@ func (l *MGU) Crossover(g1, g2 evolve.Genome) {
 	l1 := g1.(*MGU)
 	l2 := g2.(*MGU)
 
-	crossoverMatrix(&l.Wh, &l1.Wh, &l2.Wh)
-	crossoverMatrix(&l.Bh, &l1.Bh, &l2.Bh)
+	crossoverMatrix(&l.Wf, &l1.Wf, &l2.Wf)
 	crossoverMatrix(&l.Uf, &l1.Uf, &l2.Uf)
 	crossoverMatrix(&l.Bf, &l1.Bf, &l2.Bf)
+
+	crossoverMatrix(&l.Wh, &l1.Wh, &l2.Wh)
+	crossoverMatrix(&l.Uh, &l1.Uh, &l2.Uh)
+	crossoverMatrix(&l.Bh, &l1.Bh, &l2.Bh)
 }
 
 // Mutate mutates the genome
 func (l *MGU) Mutate() {
 	const rate = 0.05
 
-	mutateWeights(l.Wh.Data, rate)
+	mutateWeights(l.Wf.Data, rate)
 	mutateWeights(l.Uf.Data, rate)
-
-	mutateBias(l.Bh.Data, rate)
 	mutateBias(l.Bf.Data, rate)
+
+	mutateWeights(l.Wh.Data, rate)
+	mutateWeights(l.Uh.Data, rate)
+	mutateBias(l.Bh.Data, rate)
 }
 
 func (l *MGU) Reset() {
--- neural/layer/mgu_test.go
+++ neural/layer/mgu_test.go
@@ -9,11 +9,16 @@ import (
 
 func TestMGUUpdate(t *testing.T) {
 	m := &MGU{
+		Wf: math32.NewMatrix(2, 2, []float32{
+			1, 0,
+			0, 1,
+		}),
 		Wh: math32.NewMatrix(2, 2, []float32{
 			1, 0,
 			0, 1,
 		}),
 		Uf: math32.NewMatrix(1, 2, []float32{0.5, 0.5}),
+		Uh: math32.NewMatrix(1, 2, []float32{0.1, 0.1}),
 		Bf: math32.NewMatrix(1, 2, []float32{0, 0}),
 		Bh: math32.NewMatrix(1, 2, []float32{0, 0}),
 		h:  math32.NewMatrix(1, 2, nil),
@@ -25,28 +30,39 @@ func TestMGUUpdate(t *testing.T) {
 
 	// first step
 	out1 := m.Update(&dst, &x)
-	f := []float32{0.5, 0.5}
-	cand := make([]float32, 2)
-	math32.Matmul(&math32.Matrix{Rows: 1, Cols: 2, Data: cand}, &x, &m.Wh)
-	math32.Tanh(cand)
-	expected1 := []float32{
-		f[0]*0 + (1-f[0])*cand[0],
-		f[1]*0 + (1-f[1])*cand[1],
-	}
+	expected1 := manualMGUStep(&m.Wf, &m.Uf, &m.Bf, &m.Wh, &m.Uh, &m.Bh, x.Data, []float32{0, 0})
 	assert.InDeltaSlice(t, expected1, out1.Data, 1e-5)
 
 	// second step
-	cand2 := make([]float32, 2)
-	math32.Matmul(&math32.Matrix{Rows: 1, Cols: 2, Data: cand2}, &x, &m.Wh)
-	math32.Tanh(cand2)
-	f2 := make([]float32, 2)
-	copy(f2, m.h.Data)
-	math32.Mul(f2, m.Uf.Data)
-	math32.Sigmoid(f2)
-	expected2 := []float32{
-		f2[0]*out1.Data[0] + (1-f2[0])*cand2[0],
-		f2[1]*out1.Data[1] + (1-f2[1])*cand2[1],
-	}
+	prev := append([]float32(nil), out1.Data...)
+	expected2 := manualMGUStep(&m.Wf, &m.Uf, &m.Bf, &m.Wh, &m.Uh, &m.Bh, x.Data, prev)
 	out2 := m.Update(&dst, &x)
 	assert.InDeltaSlice(t, expected2, out2.Data, 1e-5)
+}
+
+func manualMGUStep(Wf, Uf, Bf, Wh, Uh, Bh *math32.Matrix, x, hPrev []float32) []float32 {
+	mx := math32.Matrix{Rows: 1, Cols: len(x), Data: append([]float32(nil), x...)}
+
+	f := math32.NewMatrix(1, len(hPrev), nil)
+	math32.Matmul(&f, &mx, Wf)
+	tmp := append([]float32(nil), hPrev...)
+	math32.Mul(tmp, Uf.Data)
+	math32.Add(f.Data, tmp)
+	math32.Add(f.Data, Bf.Data)
+	math32.Sigmoid(f.Data)
+
+	cand := math32.NewMatrix(1, len(hPrev), nil)
+	math32.Matmul(&cand, &mx, Wh)
+	tmp = append([]float32(nil), hPrev...)
+	math32.Mul(tmp, f.Data)
+	math32.Mul(tmp, Uh.Data)
+	math32.Add(cand.Data, tmp)
+	math32.Add(cand.Data, Bh.Data)
+	math32.Tanh(cand.Data)
+
+	out := make([]float32, len(hPrev))
+	for i := range out {
+		out[i] = (1-f.Data[i])*hPrev[i] + f.Data[i]*cand.Data[i]
+	}
+	return out
 }
